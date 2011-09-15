84679b5cd3c91e6e58b5bb93dc6135966a5cb82a
David Golden <dagolden@cpan.org>
2011-09-14 22:18:04 -0400

    dzilify distribution

--- lib/File/pushd.pm
+++ lib/File/pushd.pm
@@ -1,12 +1,14 @@
-package File::pushd;
-
-$VERSION = '1.01';
-@EXPORT  = qw( pushd tempd );
-@ISA     = qw( Exporter );
-
 use 5.004;
 use strict;
 BEGIN{ if (not $] < 5.006) { require warnings; warnings->import } }
+package File::pushd;
+# ABSTRACT: change directory temporarily for a limited scope
+# VERSION
+
+use vars qw/@EXPORT @ISA/;
+@EXPORT  = qw( pushd tempd );
+@ISA     = qw( Exporter );
+
 use Exporter;
 use Carp;
 use Cwd         qw( cwd abs_path );
@@ -88,18 +90,11 @@ sub DESTROY {
     }
 }
 
-1; #this line is important and will help the module return a true value
+1;
+
 __END__
 
 =begin wikidoc
-
-= NAME
-
-File::pushd - change directory temporarily for a limited scope
-
-= VERSION
-
-This documentation describes version %%VERSION%%.
 
 = SYNOPSIS
 
@@ -199,38 +194,7 @@ otherwise.
 
 * [File::chdir]
 
-= BUGS
-
-Please report any bugs or feature using the CPAN Request Tracker.  
-Bugs can be submitted through the web interface at 
-[http://rt.cpan.org/Dist/Display.html?Queue=File-pushd]
-
-When submitting a bug or request, please include a test-file or a patch to an
-existing test-file that illustrates the bug or desired feature.
-
-= AUTHOR
-
-David A. Golden (DAGOLDEN)
-
-= COPYRIGHT AND LICENSE
-
-Copyright (c) 2005, 2006, 2007 by David A. Golden
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at 
-[http://www.apache.org/licenses/LICENSE-2.0]
-
-Files produced as output though the use of this software, including
-generated copies of boilerplate templates provided with this software,
-shall not be considered Derivative Works, but shall be considered the
-original work of the Licensor.
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-
 =end wikidoc
+
+=cut
 
--- xt/pod.t
+++
@@ -1,6 +0,0 @@
-use Test::More;
-my $min_tp = 1.22;
-eval "use Test::Pod $min_tp";
-plan skip_all => "Test::Pod $min_tp required for testing Pod" if $@;
-
-all_pod_files_ok();
